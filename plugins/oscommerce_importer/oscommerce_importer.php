66c3770dba9322e49d4f178389d142a576c36805
sea14 <pianocat14@gmail.com>
2013-12-16 18:37:34 -0500

    doing a plugin tutorial

---
+++ plugins/oscommerce_importer/oscommerce_importer.php
@@ -0,0 +1,10 @@
+<?php
+     /*
+     Plugin Name: OSCommerce Product Display Tutorial
+     Plugin URI:
+     Description: A test-run of the tutorial for this plugin, via Net Tuts Plus
+     Author: Sharon A.
+     Version: 1.0
+     Author URI: http://www.sharon-elizabeth.com/
+     */
+?>
